609ff19564390d401b534a2a35503e2d5c938ea4
benjacoblee <benjacoblee@gmail.com>
2020-05-21 12:46:04 +0800

    feat: fetch and render boards

--- components/init.js
+++ components/init.js
@@ -1,3 +1,16 @@
-window.addEventListener("DOMContentLoaded", ()=> {
-  console.log("LOADED")
-})
+window.addEventListener("DOMContentLoaded", () => {
+  const fetchBoards = async () => {
+    const res = await fetch("http://localhost:3000/boards");
+    const json = await res.json();
+
+    json.forEach(({ id, title }) => {
+      const boardContainer = document.querySelector(".board-container");
+      const board = document.createElement("board-element");
+      board.setAttribute("id", id)
+      board.setAttribute("title", title);
+      boardContainer.appendChild(board);
+    });
+  };
+
+  fetchBoards();
+});
