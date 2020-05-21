d5cf4cc7032b975380ad07e59d82d1a3ef48f36a
benjacoblee <benjacoblee@gmail.com>
2020-05-21 15:45:15 +0800

    chore: figure out how to append child to parent component

---
+++ components/addTask.js
@@ -0,0 +1,39 @@
+class AddTask extends HTMLElement {
+  constructor() {
+    super();
+    this.shadow = this.attachShadow({ mode: "open" });
+    this._boardId = this.getAttribute("id");
+  }
+
+  connectedCallback() {
+    this.render();
+
+    let clickElement = this.shadowRoot.querySelector("span");
+    clickElement.addEventListener("click", () => {
+      const inputElement = document.createElement("input");
+      inputElement.type = "text";
+      clickElement = inputElement;
+    });
+  }
+
+  render() {
+    const template = `
+    <style>
+      div {
+        display: block;
+        background-color: #FFFFFF;
+        border: 1px solid black;
+        border-radius: 2px;
+        margin: 2px;
+        padding: 5px;
+      }
+      </style>
+     <div>
+    <p>Add a card <span>+</span></p>
+    </div>`;
+
+    this.shadow.innerHTML = template;
+  }
+}
+
+customElements.define("add-task", AddTask);
