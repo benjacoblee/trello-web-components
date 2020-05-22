5ad62201e27739208bde662bc8c3cb0c09fa0ad1
benjacoblee <benjacoblee@gmail.com>
2020-05-22 12:09:51 +0800

    feat: default display description to none. show description onclick

--- components/tasks.js
+++ components/tasks.js
@@ -2,18 +2,32 @@ class TaskElement extends HTMLElement {
   constructor() {
     super();
     this.shadow = this.attachShadow({ mode: "open" });
+    this._taskTitle;
+    this._showTask = false;
   }
+
+  handleDeleteClick = async (e) => {
+    const taskId = e.target.id;
+    await fetch(`http://localhost:3000/tasks/${taskId}`, {
+      method: "DELETE"
+    });
+  };
+
+  handleDisplayClick = () => {
+    this._showTask ? (this._showTask = false) : (this._showTask = true);
+    this._showTask
+      ? (this._taskDescription.style.display = "block")
+      : (this._taskDescription.style.display = "none");
+  };
 
   connectedCallback() {
     this.render();
+    this._taskTitle = this.shadow.querySelector(".title");
+    this._taskDescription = this.shadow.querySelector(".description");
     const deleteButtons = this.shadow.querySelectorAll("span");
+    this._taskTitle.addEventListener("click", this.handleDisplayClick);
     deleteButtons.forEach((button) => {
-      button.addEventListener("click", async (e) => {
-        const taskId = e.target.id;
-        await fetch(`http://localhost:3000/tasks/${taskId}`, {
-          method: "DELETE"
-        });
-      });
+      button.addEventListener("click", this.handleDeleteClick);
     });
   }
 
@@ -32,13 +46,28 @@ class TaskElement extends HTMLElement {
      span:hover {
       cursor: pointer;
      }
+
+     p {
+       margin: 0;
+     }
+
+     p.title {
+       font-weight: bold;
+     }
+
+     p.title:hover {
+       cursor: pointer;
+     }
+
+     .description {
+       display: none;
+     }
     </style>
     <div class=tasks>
-    <strong>${this.getAttribute("title")}</strong><span id=${this.getAttribute(
+    <p class="title">${this.getAttribute("title")} <span id=${this.getAttribute(
       "id"
-    )}> x</span>
-    <br>
-    ${this.getAttribute("description")}
+    )}> x</span></p>
+    <p class="description">${this.getAttribute("description")}</p>
     </div>`;
     this.shadow.innerHTML = template;
   }
